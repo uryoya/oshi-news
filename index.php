50445cf891cceca581610f0b240e052ae9e84ca6
uryoya <uryoya.ur@gmail.com>
2016-07-15 14:10:43 +0900

    POSTとGETの処理を分けた

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$check_word = "POSTを受け取りました";
+} else {
+	$check_word = "GETを受け取りました";
+}
 
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta>
+		<meta charset="UTF-8">
+		<title>推しニュース（仮称）</title>
 	</head>
 	<body>
+		<?php echo $check_word ?>
 	</body>
 </html>
 
