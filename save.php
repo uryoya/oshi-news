bf8d6c0e29931c3f92569aa433570c9a0418e3e3
uryoya <uryoya.ur@gmail.com>
2016-07-17 17:34:02 +0900

    重複するURLはvoteをインクリメントするようにした

--- save.php
+++ save.php
@@ -5,15 +5,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	// - URLをDBに保存する
 	// - ランキングを更新する
 	// - 最新版のランキングを表示する
-	
 	$dbh = new PDO("pgsql:dbname=oshinews;host=localhost", "uryoya", "pass");
-	// urlの登録
-	$url = $_POST["url"];
+	$url = $_POST["url"]; 
+	if (!$url) {
+		// index.phpにリダイレクトさせる
+		header("HTTP/1.1 301 Moved Premanently");
+		header("Location: http://localhost:8888/index.php");
+		exit();
+ 	}
 	$datetime = date("Y-m-d H:i:s");
-	$sql = "INSERT INTO urls (url, lank, vote, datetime) VALUES ('".$url."', 1, 1, '".$datetime."');";
-	$dbh->exec($sql);
-
+	
+	// urlの登録
+	// 既にあるURLの確認
+	$sth= $dbh->prepare( "SELECT id FROM urls WHERE '".$url."' = url;");
+	$sth->execute();
+	$results = $sth->fetch(PDO::FETCH_ASSOC);
+	foreach ($results as $result) {
+		error_log($result, 0);
+	}
+	if ($result > 0) {
+		$dbh->exec(
+			"UPDATE urls ".
+			"SET vote=vote+1, datetime='".$datetime."' ".
+			"WHERE id=".$result.";"
+		);
+	} else {
+		$sql = "INSERT INTO urls (url, lank, vote, datetime) VALUES ('".$url."', 1, 1, '".$datetime."');";
+		$dbh->exec($sql);
+	}
 }
 // index.phpにリダイレクトさせる
 header("HTTP/1.1 301 Moved Premanently");
-header("Location: http://localhost/oshi-news/index.php");
+header("Location: http://localhost:8888/index.php");
