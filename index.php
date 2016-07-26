71a97c4fad953454e4e02903925d904828149e68
uryoya <uryoya.ur@gmail.com>
2016-07-27 03:21:06 +0900

    リスト表示のところをいい感じに設定した

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 require "./utils.php";
 
 $ranking = new NewsRanking();
-$ranking = $ranking->get_ranking(10);
+$ranking = $ranking->get_ranking(100);
 ?>
 
 <!DOCTYPE html>
@@ -13,7 +13,7 @@ $ranking = $ranking->get_ranking(10);
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viesport" content="with=device-widh, initial-scale=1">
-		<title>推しニュース（仮称）</title>
+		<title>推しニュース!</title>
 		<!-- import css of bootstrap -->
 		<link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
 		<!-- import jquery -->
@@ -26,23 +26,27 @@ $ranking = $ranking->get_ranking(10);
 			<div class="row">
 				<div class="col-sm-3"></div>
 				<div class="col-sm-6">
-					<h1>推しニュース!</h1>
-					<p>自分の今気になっているニュースを共有しよう！</p>
+					<header>
+						<h1>推しニュース!</h1>
+						<p>自分の今気になっているニュースを共有しよう！</p>
 					<form action="save.php" method="POST">
 						<div class="input-group">
-							<input class="form-control" type="url" name="url" placeholder="e.g. https://example.com">
+							<input class="form-control" type="url" name="url" placeholder="e.g. https://example.com/path/to/news">
 							<span class="input-group-btn">
 								<button class="btn btn-primary btn-default" type="submit">Submit!</button>
 							</span>
 						</div>
 					</form>
-					<ol>
+					<br/>
+					</header>
+
+					<ul class="list-group">
 						<?php
 						foreach ($ranking as $row) {
-							echo '<li><a href="'.$row['url'].'">'.$row['title'].'</a></li>' ;
+							echo '<li class="list-group-item"><a href="'.$row['url'].'" target="_blank">'.$row['title'].'</a></li>'."\n";
 						}
 						?>
-					</ol>
+					</ul>
 				</div>
 				<div class="col-sm-3"></div>
 			</div>
