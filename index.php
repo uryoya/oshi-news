4b91d6cac681a766f9d6dd174c923d1983accf84
uryoya <uryoya.ur@gmail.com>
2016-07-27 01:33:48 +0900

    bootstrapの読み込み設定を書いた

--- index.php
+++ index.php
@@ -8,10 +8,18 @@ $ranking = $ranking->get_ranking(10);
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 	<head>
 		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viesport" content="with=device-widh, initial-scale=1">
 		<title>推しニュース（仮称）</title>
+		<!-- import css of bootstrap -->
+		<link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+		<!-- import jquery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<!-- import javascript of bootstrap -->
+		<script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<h1>推しニュース!</h1>
